da127e90a7eda052bcb469a909b128a36e2e7a6c
eyuste-FS <eyuste@futurespace.es>
2023-10-31 11:11:37 +0100

    SQL crear V1.1 y poblar V0.1 (correcciones)

--- sqlscripts/populateDB.sql
+++ sqlscripts/populateDB.sql
@@ -3,7 +3,8 @@ USE PRACTICA;
 
 insert into PRACTICA.EM_EMPLEADOS values
 	(NULL, "nif", "nombre", "apellido1", "apellido2", "2000-01-01", "tele1", "tele2", "email", "2020-10-20", NULL, 'C', 'N'),
-	(NULL, "nif2", "nombre2", "apellido12", "apellido22", "2000-01-02", "tele12", "tele22", "email2", "2020-10-22", NULL, 'C', 'N');
+	(NULL, "nif2", "nombre2", "apellido12", "apellido22", "2000-01-02", "tele12", "tele22", "email2", "2020-10-22", NULL, 'C', 'S'),
+	(NULL, "nif3", "nombre3", "apellido13", "apellido23", "2000-01-03", "tele13", "tele23", "email3", "2020-10-23", NULL, 'S', 'S');
 
 insert into PRACTICA.PR_PROYECTOS values
 	(NULL, "descripcion", "2021-02-15", NULL, NULL, "lugar", "observaciones"),
@@ -16,11 +17,7 @@ insert into PRACTICA.PR_EMPLEADOS_PROYECTO values
     (2, 2, "2022-10-04"),
     (2, 3, "2022-10-04");
 
-insert into PRACTICA.PR_EMPLEADOS_PROYECTO values
-	(2, 2, NULL);  # DEBE FALLAR por colision de PK
-
 
 select * from PRACTICA.EM_EMPLEADOS;
 select * from PRACTICA.PR_PROYECTOS;
 select * from PRACTICA.PR_EMPLEADOS_PROYECTO;
-

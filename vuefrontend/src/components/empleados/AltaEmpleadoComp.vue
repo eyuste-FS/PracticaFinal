efb9e96cd77deb01c9a9cbb146ab472379502b9b
eyuste-FS <eyuste@futurespace.es>
2023-11-07 10:14:46 +0100

    Front: Alta proyecto + retoque en alta de empleado

--- vuefrontend/src/components/empleados/AltaEmpleadoComp.vue
+++ vuefrontend/src/components/empleados/AltaEmpleadoComp.vue
@@ -9,28 +9,26 @@
         <v-col>
         <v-row>
             <v-text-field
-                label="NIF" v-model="nif" 
-                min=1
-                max=20
+                label="NIF" v-model="nif" :maxlength="max.nif"
                 ></v-text-field>
             <v-spacer></v-spacer>
         </v-row>
         <v-row>
             <v-col>
-            <v-text-field label="Nombre" v-model="nombre" min=1 max=30></v-text-field>
-            <v-text-field label="Primer apellido" v-model="primerApellido" min=1 max=40></v-text-field>
-            <v-text-field label="Segundo apellido" v-model="segundoApellido" min=1 max=40></v-text-field>
+            <v-text-field label="Nombre" v-model="nombre" :maxlength="max.nombre"></v-text-field>
+            <v-text-field label="Primer apellido" v-model="primerApellido" :min="min.primerApellido" :maxlength="max.primerApellido"></v-text-field>
+            <v-text-field label="Segundo apellido" v-model="segundoApellido" :min="min.segundoApellido" :maxlength="max.segundoApellido"></v-text-field>
             </v-col>
             <v-col>
-            <v-text-field label="Primer teléfono de contacto" v-model="telefono1" min=1 max=12></v-text-field>
-            <v-text-field label="Segundo teléfono de contacto" v-model="telefono2" min=1 max=12></v-text-field>
+            <v-text-field label="Primer teléfono de contacto" v-model="telefono1" :maxlength="max.telefono1"></v-text-field>
+            <v-text-field label="Segundo teléfono de contacto" v-model="telefono2" :maxlength="max.telefono2"></v-text-field>
             </v-col>
             <v-spacer></v-spacer>
             <v-spacer></v-spacer>
         </v-row>
 
         <v-row>
-            <v-text-field label="Email" v-model="email" min=5 max=40></v-text-field>
+            <v-text-field label="Email" v-model="email" :maxlength="max.email"></v-text-field>
             <v-spacer></v-spacer><v-spacer></v-spacer>
         </v-row>
 
@@ -62,11 +60,11 @@
         <v-row>
             <v-col>
                 <h4>Fecha de nacimiento</h4>
-                <v-date-picker v-model="fechaNacimiento" elevation="10"> </v-date-picker>
+                <v-date-picker v-model="fechaNacimiento" elevation="10" :max="yearsAgo(15)"> </v-date-picker>
             </v-col>
             <v-col>
                 <h4>Fecha de alta</h4>
-                <v-date-picker v-model="fechaAlta" elevation="10"> </v-date-picker>
+                <v-date-picker v-model="fechaAlta" elevation="10" :min="today"> </v-date-picker>
             </v-col>
             <v-spacer></v-spacer>
         </v-row>
@@ -105,7 +103,7 @@ import axios from 'axios';
 import Swal from 'sweetalert2';
 
 export default {
-  name: "alta-empleados-comp",
+  name: "alta-empleado-comp",
 
   data: () => ({
     nif: "",
@@ -119,9 +117,38 @@ export default {
     email: "",
     estadoCivil: "", // [Soltero/Casado]
     servicioMilitar: "", // [Si/No]
-  }),
+
+    max: {
+        nif: 20,
+        nombre: 30,
+        primerApellido: 40,
+        segundoApellido: 40,
+        telefono1: 12,
+        telefono2: 12,
+        email: 40,
+        fechaAlta: 10,
+        fechaNacimiento: 10,
+    },
+    min: {
+        nif: 1,
+        nombre: 1,
+        primerApellido: 1,
+        segundoApellido: 1,
+        telefono1: 3,
+        telefono2: 3,
+        email: 5,
+        fechaAlta: 10,
+        fechaNacimiento: 10,
+        estadoCivil: 6,
+        servicioMilitar: 2,
+    }
+
+}),
 
   computed: {
+    today(){
+      return new Date().toISOString().substring(0, 10);
+    },
     formulario(){
         return {
             nif: this.nif,
@@ -140,6 +167,11 @@ export default {
   },
 
   methods:{
+    yearsAgo(n){
+        let today = new Date();
+        today.setFullYear(today.getFullYear() - n);
+        return today.toISOString().substring(0, 10);
+    },
 
     wrongFields(){
         const fechaRe = new RegExp("^[0-9]{4}-[0-9]{2}-[0-9]{2}$");
@@ -148,35 +180,35 @@ export default {
         let emptyfields = [];
         let wrong = [];
 
-        if (this.nif.length < 1) emptyfields.push('Nif');
-        else if (this.nif.length > 20) wrong.push('Nif demasiado largo');
-        if (this.nombre < 1) emptyfields.push("Nombre");
-        else if (this.nombre.length > 30) wrong.push('Nombre demasiado largo');
-        if (this.primerApellido < 1) emptyfields.push("1º Apellido");
-        else if (this.primerApellido.length > 40) wrong.push('1º Apellido demasiado largo');
-        if (this.segundoApellido < 1) emptyfields.push("2º Apellido");
-        else if (this.segundoApellido.length > 40) wrong.push('2º Apellido demasiado largo');
+        if (this.nif.length < this.min.nif) emptyfields.push('Nif');
+        else if (this.nif.length > this.max.nif) wrong.push('Nif demasiado largo');
+        if (this.nombre.length < this.min.nombre) emptyfields.push("Nombre");
+        else if (this.nombre.length > this.max.nombre) wrong.push('Nombre demasiado largo');
+        if (this.primerApellido.length < this.min.primerApellido) emptyfields.push("1º Apellido");
+        else if (this.primerApellido.length > this.max.primerApellido) wrong.push('1º Apellido demasiado largo');
+        if (this.segundoApellido.length < this.min.segundoApellido) emptyfields.push("2º Apellido");
+        else if (this.segundoApellido.length > this.max.segundoApellido) wrong.push('2º Apellido demasiado largo');
         
-        if (this.fechaNacimiento < 1) emptyfields.push("Fecha de nacimiento");
+        if (this.fechaNacimiento.length < this.min.fechaNacimiento) emptyfields.push("Fecha de nacimiento");
         else if (!fechaRe.test(this.fechaNacimiento)) wrong.push("Fecha de nacimiento: formato incorrecto");
         
-        if (this.fechaAlta < 1) emptyfields.push("Fecha de alta");
+        if (this.fechaAlta.length < this.min.fechaAlta) emptyfields.push("Fecha de alta");
         else if (!fechaRe.test(this.fechaAlta)) wrong.push("Fecha de alta: formato incorrecto");
         
-        if (this.telefono1 < 1) emptyfields.push("1º Teléfono");
+        if (this.telefono1.length < this.min.telefono1) emptyfields.push("1º Teléfono");
         else if (!telefonoRe.test(this.telefono1)) wrong.push("1º Teléfono: formato incorrecto");
 
-        if (this.telefono2 < 1) emptyfields.push("2º Teléfono");
+        if (this.telefono2.length < this.min.telefono2) emptyfields.push("2º Teléfono");
         else if (!telefonoRe.test(this.telefono2)) wrong.push("1º Teléfono: formato incorrecto");
         
-        if (this.email < 1) emptyfields.push("Email");
+        if (this.email.length < this.min.email) emptyfields.push("Email");
         else if (!emailRe.test(this.email)) wrong.push("Email: formato incorrecto");
-        else if (this.email.length > 40) wrong.push("Email: demasiado largo")
+        else if (this.email.length > this.max.email) wrong.push("Email: demasiado largo")
 
-        if (this.estadoCivil < 1) emptyfields.push("Estado civil");
+        if (this.estadoCivil.length < this.min.estadoCivil) emptyfields.push("Estado civil");
         else if (this.estadoCivil != "Casado" && this.estadoCivil != "Soltero") wrong.push("Estado civil fuera de rango");
 
-        if (this.servicioMilitar < 1) emptyfields.push("Servicio militar");
+        if (this.servicioMilitar.length < this.min.servicioMilitar) emptyfields.push("Servicio militar");
         else if (this.servicioMilitar != "Si" && this.servicioMilitar != "No") wrong.push("Servicio militar fuera de rango");
 
         return [emptyfields, wrong];
--- vuefrontend/src/components/proyectos/AltaProyectoComp.vue
+++ vuefrontend/src/components/proyectos/AltaProyectoComp.vue
@@ -1,17 +1,189 @@
 <template>
   <v-container>
+    <v-btn @click="$router.back()">
+        <span class="mdi mdi-arrow-left"></span>
+    </v-btn>
+    <br>
+    <br>
+    <br>
+    <v-form>
 
-    Alta proyecto
+      <v-row>
+        <v-col>
+          <v-row>
+              <v-textarea label="Descripción del proyecto" v-model="descripcion" :maxlength="max.descripcion"></v-textarea>
+          </v-row>
+          <v-row>
+              <v-text-field label="Lugar" v-model="lugar" :maxlength="max.lugar"></v-text-field>
+          </v-row>
+          <v-row>
+              <v-textarea label="Observaciones" v-model="observaciones" :maxlength="max.observaciones"></v-textarea>
+          </v-row>
+
+        </v-col>
+        <v-col>
+            <h4>Fecha de inicio</h4>
+            <v-date-picker v-model="fechaInicio" elevation="10" :min="today"> </v-date-picker>
+        </v-col>
+      </v-row>
+      <br>
+
+      <v-container style="padding: 30px; justify-content: space-between;">
+        <v-row>
+          <v-spacer></v-spacer>
+          <v-btn
+            class="white--text"
+            color="blue"
+            @click="aceptar()"
+          >
+            Aceptar
+          </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn
+            class="white--text"
+            color="red"
+            @click="cancelar()">
+            Cancelar
+          </v-btn>
+          <v-spacer></v-spacer>
+        </v-row>
+
+      </v-container>
+    </v-form>
 
   </v-container>
 </template>
 
 
 <script>
+import axios from 'axios';
+import Swal from 'sweetalert2';
+
 export default {
   name: "alta-proyecto-comp",
 
   data: () => ({
+    descripcion: "",
+    fechaInicio: "",
+    lugar: "",  // 30
+    observaciones: "",  // 300
+
+    max: {
+      descripcion: 125,
+      fechaInicio: 10,
+      lugar: 30,
+      observaciones: 300,
+    },
+
+    min: {
+      descripcion: 1,
+      fechaInicio: 10,
+      lugar: 1,
+      observaciones: 1,
+    },
+
   }),
+
+  computed: {
+    today(){
+      return new Date().toISOString().substring(0, 10);
+    },
+    formulario(){
+        return {
+          descripcion: this.descripcion,
+          fechaInicio: this.fechaInicio,
+          lugar: this.lugar,
+          observaciones: this.observaciones,
+        };
+    },
+  },
+
+  methods:{
+
+    wrongFields(){
+        const fechaRe = new RegExp("^[0-9]{4}-[0-9]{2}-[0-9]{2}$");
+        let emptyfields = [];
+        let wrong = [];
+
+        if (this.descripcion.length < this.min.descripcion) emptyfields.push('Descripción');
+        else if (this.descripcion.length > this.max.descripcion) wrong.push('Descripción demasiado larga');
+
+        if (this.fechaInicio.length < this.min.fechaInicio) emptyfields.push("Fecha de incio");
+        else if (!fechaRe.test(this.fechaInicio)) wrong.push("Fecha de inicio: formato incorrecto");
+        
+        if (this.lugar.length < this.min.lugar) emptyfields.push("Lugar");
+        else if (this.lugar.length > this.max.lugar) wrong.push("Lugar: demasiado largo")
+
+        if (this.observaciones.length < this.min.observaciones) emptyfields.push("Observaciones");
+        else if (this.observaciones.length > this.max.observaciones) wrong.push("Observaciones: demasiado largas")
+
+        return [emptyfields, wrong];
+    },
+
+    aceptar(){
+        console.log(this.formulario);
+        // checks
+        let [emptyFields, wrong] = this.wrongFields();
+        console.log(emptyFields, wrong);
+        if (emptyFields.length > 0 || wrong.length > 0){
+            let estr = emptyFields.length > 0?
+                ("<b>Hay campos obligatorios sin completar:</b> <ul>" + emptyFields
+                .map(f => "<li> - " + f + "</li>")
+                .reduce((a, b) => a + b) + "</ul>") : "";
+            let wstr = wrong.length > 0 ? 
+                ("<b>Error en los campos:</b> <ul>" + wrong
+                    .map(f => "<li> - " + f + "</li>")
+                    .reduce((a, b) => a + b) + "</ul>") :
+                "";
+
+            Swal.fire({
+                titleText: "Es obligatorio introducir todos los datos para dar de alta un nuevo proyecto",
+                html:  "<div style='text-align: left'>"+ estr + wstr + "</div>",
+                icon: "error",
+            })
+            return;
+        }
+
+        axios.put(this.apiBaseUrl + 'proyecto', this.formulario)
+            .then(() => {
+                this.$router.back();
+                
+                Swal.fire({
+                    titleText: "Nuevo proyecto dado de alta",
+                    text: this.descripcion,
+                    icon: "success",
+                })
+            })
+            .catch(resp => {
+                console.log(resp)
+                Swal.fire({
+                    titleText: "Error",
+                    html: resp,
+                    icon: "error",
+                })
+            });
+
+    },
+
+    cancelar(){
+        //back
+        this.$router.back();
+    },
+
+  },
+
+  mounted(){
+  },
+
 };
+
 </script>
+<!--
+Esperado por el PUT {
+  "descripcion": "Test description",  // 125
+  "fechaInicio": "2023-11-03",
+  "lugar": "Madrid",  // 30
+  "observaciones": "Ninguna"  // 300
+}
+12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345
+-->
